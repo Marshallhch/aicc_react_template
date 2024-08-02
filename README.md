c912a2493e69ecbfa8fbac592388f3603155daaa
marshallhch <marshallhch@gmail.com>
2024-08-02 17:49:27 +0900

    create hero and features sections

--- README.md
+++ README.md
@@ -1,31 +1,21 @@
 <h1 align="center">
-  <a href="#">
-    Marshall From AICC
+  <a href="">
+    <img src="./docs/images/logo.svg" alt="Logo" width="125" height="125">
   </a>
 </h1>
+
+<h4 align="center">
+    Noveled From AICC 
+</h4>
 
 <div align="center">
   React UI Template for exercise - Multi Page Template
   <br />
   <br />
-  <a href="#">Report a Bug</a>
+  <a href="">Report a Bug</a>
   ·
-  <a href="#">Request a Feature</a>
+  <a href="">Request a Feature</a>
   .
-  <a href="#">Ask a Question</a>
+  <a href="">Ask a Question</a>
 </div>
 
-<div align="center">
-<br />
-  
-</div>
-
-<details open="open">
-<summary>Table of Contents</summary>
-
-- [Setting](#setting)
-  - [Built With](#built-with)
-
-</details>
-
-## Setting
--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './components/Home';
+import Features from './components/Features';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import Signin from './components/Auth/Signin';
+import Signup from './components/Auth/Signup';
+
 const App = () => {
   return (
-    <div className="App">
-      <div className="kr">첫 페이지 구성</div>
-      <div className="en">Create First Page</div>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Navbar />
+
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/features" element={<Features />} />
+
+          <Route path="/signin" element={<Signin />} />
+          <Route path="/signup" element={<Signup />} />
+        </Routes>
+
+        <Footer />
+      </div>
+    </BrowserRouter>
   );
 };
 
